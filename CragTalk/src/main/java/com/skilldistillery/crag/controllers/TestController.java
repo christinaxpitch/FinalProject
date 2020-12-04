c208c0c29b74d46e0137d638a3b091706a5c02ac
Tj Laughlin <tlaughl1@gmail.com>
2020-12-04 15:41:43 -0700

    added cross origin mapping to test controller

--- CragTalk/src/main/java/com/skilldistillery/crag/controllers/TestController.java
+++ CragTalk/src/main/java/com/skilldistillery/crag/controllers/TestController.java
@@ -1,13 +1,16 @@
 package com.skilldistillery.crag.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin({ "*", "http://localhost:4210" })
 @RequestMapping("api")
 @RestController
 public class TestController {
 
+	
 	@GetMapping("ping")
 	public String ping() {
 		return "pong";
