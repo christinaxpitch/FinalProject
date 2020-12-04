5536e4eb80f4f25d7add402fdb14fa99264156ba
git-jwolcott <jeanlouisentx@yahoo.com>
2020-12-04 13:37:18 -0700

    Create Spring Boot REST project

---
+++ CragTalk/src/main/java/com/skilldistillery/crag/CragTalkApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.crag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CragTalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CragTalkApplication.class, args);
+	}
+
+}
---
+++ CragTalk/src/test/java/com/skilldistillery/crag/CragTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.crag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CragTalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
