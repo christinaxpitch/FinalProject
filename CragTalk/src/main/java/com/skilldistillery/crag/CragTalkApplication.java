3ad12260f8cc478994ada99bcc5b0045eb4ff314
git-jwolcott <jeanlouisentx@yahoo.com>
2020-12-05 16:49:40 -0700

    commented out security stuff

--- CragTalk/src/main/java/com/skilldistillery/crag/CragTalkApplication.java
+++ CragTalk/src/main/java/com/skilldistillery/crag/CragTalkApplication.java
@@ -3,8 +3,8 @@ package com.skilldistillery.crag;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class CragTalkApplication {
@@ -13,9 +13,9 @@ public class CragTalkApplication {
 		SpringApplication.run(CragTalkApplication.class, args);
 	}
 	
-	@Bean
-	public PasswordEncoder configurePasswordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+//	@Bean
+//	public PasswordEncoder configurePasswordEncoder() {
+//		return new BCryptPasswordEncoder();
+//	}
 
 }
