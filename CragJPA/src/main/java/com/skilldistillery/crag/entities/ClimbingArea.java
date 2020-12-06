bae3cad97409180df4fe7195763bc10bf680d6e5
Pierce Steward <pierce.steward@gmail.com>
2020-12-06 10:40:19 -0700

    Finished climbingAreaRepo and Impl

--- CragJPA/src/main/java/com/skilldistillery/crag/entities/ClimbingArea.java
+++ CragJPA/src/main/java/com/skilldistillery/crag/entities/ClimbingArea.java
@@ -75,14 +75,6 @@ public class ClimbingArea {
 		this.imgUrl = imgUrl;
 	}
 
-//	public Location getLocation() {
-//		return location;
-//	}
-//
-//	public void setLocation(Location location) {
-//		this.location = location;
-//	}
-
 	@Override
 	public int hashCode() {
 		final int prime = 31;
