006ccdac0f1b7a8e24b30662ac80ab52aa9b1fd3
Tj Laughlin <tlaughl1@gmail.com>
2020-12-07 09:29:19 -0700

    changes to gear repo

--- CragTalk/src/main/java/com/skilldistillery/crag/repositories/GearRepository.java
+++ CragTalk/src/main/java/com/skilldistillery/crag/repositories/GearRepository.java
@@ -7,8 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.skilldistillery.crag.entities.Gear;
 
 public interface GearRepository extends JpaRepository<Gear, Integer> {
-
-	Gear findByUser_UsernameAndId(String username, int todoId);
+	
 	
 	List<Gear> findByUser_Username(String username);
 	
