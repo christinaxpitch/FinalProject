0126cab30ad9a30a482f0643257c92d7545a5ba2
Christina <christina.pitch@gmail.com>
2020-12-08 15:53:17 -0700

    finished message and location models in angular

--- ngCragTalk/src/app/models/location.ts
+++ ngCragTalk/src/app/models/location.ts
@@ -1,2 +1,19 @@
 export class Location {
-}
+  id         : number;
+  senderId   : number;
+  receiverId : number;
+  messageBody: string;
+
+  constructor(
+    id?          : number,
+    senderId?    : number,
+    receiverId?  :  number,
+    messageBody? : string
+
+  ){
+    this.id          = id;
+    this.senderId    = senderId;
+    this.receiverId  = receiverId;
+    this.messageBody = messageBody;
+  }
+};
--- ngCragTalk/src/app/models/message.ts
+++ ngCragTalk/src/app/models/message.ts
@@ -1,2 +1,32 @@
+import { ClimbingArea } from './climbing-area';
+import { User } from './user';
+
 export class Message {
+  id            : number;
+  city          : string;
+  state         : string;
+  zip           : number;
+  users         : User [];
+  streetAddress : string;
+  climbingAreas : ClimbingArea []
+
+
+constructor(
+  id?             : number,
+  city?           : string,
+  state?          : string,
+  zip?            : number,
+  users?          : User [],
+  streetAddress?  : string,
+  climbingAreas?  : ClimbingArea []
+
+){
+  this.id            = id;
+  this.city          = city;
+  this.state         = state;
+  this.zip           = zip;
+  this.users         = users;
+  this.streetAddress = streetAddress;
+  this.climbingAreas = climbingAreas;
 }
+};
