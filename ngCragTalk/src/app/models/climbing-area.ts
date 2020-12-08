4495212bfe9f9d90276ca8e32678701830c8abe9
Tj Laughlin <tlaughl1@gmail.com>
2020-12-08 15:48:17 -0700

    gear, media, climbingArea, climbtype models done in angular

--- ngCragTalk/src/app/models/climbing-area.ts
+++ ngCragTalk/src/app/models/climbing-area.ts
@@ -1,2 +1,28 @@
+import { User } from './user';
+
 export class ClimbingArea {
+  id: number;
+  name: string;
+  description: string;
+  imgUrl: string;
+  users: User[];
+  location: Location;
+
+
+  constructor(
+    id?: number,
+    name?: string,
+    description?: string,
+    imgUrl?: string,
+    users?: User[],
+    location?: Location
+  ) {
+    this.id = id;
+  this.name = name;
+  this.description = description;
+  this.imgUrl = imgUrl;
+  this.users = users;
+  this.location = location;
+  }
+
 }
