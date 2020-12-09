4bd3ee0be7919e0633590edc2db3b42ff33a07d8
Pierce Steward <pierce.steward@gmail.com>
2020-12-09 16:14:05 -0700

    homepage

---
+++ CragTalk/src/main/java/com/skilldistillery/crag/controllers/MediaController.java
@@ -0,0 +1,37 @@
+package com.skilldistillery.crag.controllers;
+
+import java.security.Principal;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.skilldistillery.crag.entities.Media;
+import com.skilldistillery.crag.services.MediaService;
+
+@CrossOrigin({ "*", "http://localhost:4210" })
+@RequestMapping("api")
+@RestController
+public class MediaController {
+
+	@Autowired
+	private MediaService mediaService;
+	
+	@GetMapping("media")
+	public List<Media> index(HttpServletResponse res, Principal principal) {
+		List<Media> media = mediaService.index(principal.getName());
+		try {
+			if (media == null) {
+				res.setStatus(404);
+			}
+		} catch (Exception e) {
+			res.setStatus(400);
+		}
+		return media;
+	}
+}
