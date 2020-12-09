8cfbee11a26b6760a56acd035ab7a3552a6b5bf8
git-jwolcott <jeanlouisentx@yahoo.com>
2020-12-09 07:03:37 -0700

    updated app.component.html to include app-routing module

--- ngCragTalk/src/app/app-routing.module.ts
+++ ngCragTalk/src/app/app-routing.module.ts
@@ -3,16 +3,21 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ProfileComponent } from './components/profile/profile.component';
 import { RegisterComponent } from './components/register/register.component';
 
-const routes: Routes = [{ path: '', pathMatch: 'full', redirectTo: 'home' },
-{ path: 'home', component: HomeComponent },
-{ path: 'register', component: RegisterComponent },
-{ path: 'login', component: LoginComponent },
-{ path: '**', component: NotFoundComponent },];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'user', component: ProfileComponent },
+  { path: 'user/:userId', component: ProfileComponent },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {useHash: true})],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
