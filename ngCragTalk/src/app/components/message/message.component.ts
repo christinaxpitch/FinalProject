7a87a1bb7b18d71fee701e68b202070dff4602cd
Christina <christina.pitch@gmail.com>
2020-12-11 13:08:03 -0700

    no message is a message

--- ngCragTalk/src/app/components/message/message.component.ts
+++ ngCragTalk/src/app/components/message/message.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-message',
@@ -7,7 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessageComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private userSvc: UserService,
+    private authSvc: AuthService,
+  ) { }
 
   ngOnInit(): void {
   }
